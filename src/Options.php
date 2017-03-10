c08b26c159e28cfead360b049147148e29cfd4ea
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-03-10 19:51:31 +0100

    Options

---
+++ src/Options.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Middleware\UserAgent;
+
+final class Options
+{
+    const USER_AGENT          = 'user_agent';
+    const USER_AGENT_STRATEGY = 'user_agent_strategy';
+    const PACKAGE             = 'package';
+}
