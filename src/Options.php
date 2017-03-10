d6ab0d77842e8e6f4bb0307b88af78a728ac0be8
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-03-10 20:19:31 +0100

    Updated options

--- src/Options.php
+++ src/Options.php
@@ -4,7 +4,6 @@ namespace ApiClients\Middleware\UserAgent;
 
 final class Options
 {
-    const USER_AGENT          = 'user_agent';
-    const USER_AGENT_STRATEGY = 'user_agent_strategy';
-    const PACKAGE             = 'package';
+    const STRATEGY = 'user_agent_strategy';
+    const PACKAGE  = 'package';
 }
