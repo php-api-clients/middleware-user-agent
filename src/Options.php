17260393db9b131ad3ec8b7783a8b9fd1e68dff2
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-03-10 20:20:55 +0100

    Reordered and restored overzeulously removed option

--- src/Options.php
+++ src/Options.php
@@ -4,6 +4,7 @@ namespace ApiClients\Middleware\UserAgent;
 
 final class Options
 {
-    const STRATEGY = 'user_agent_strategy';
-    const PACKAGE  = 'package';
+    const PACKAGE    = 'package';
+    const STRATEGY   = 'user_agent_strategy';
+    const USER_AGENT = 'user_agent';
 }
